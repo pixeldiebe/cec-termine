a8d62425f2daa35c529db82b949208e5164a4f6e
pixeldiebe <48907910+pixeldiebe@users.noreply.github.com>
2020-09-23 20:27:47 +0200

    Plugin fast final
    styles und funktionen

--- cec-termine.php
+++ cec-termine.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
  *
  * @link              https://herzog-webstudios.de/
  * @since             1.0.0
@@ -15,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name:       clean event cards
  * Plugin URI:        https://herzog-webstudios.de/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Simple Termin Karten.
  * Version:           1.0.0
  * Author:            Florian Herzog
  * Author URI:        https://herzog-webstudios.de/
@@ -37,46 +31,189 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'CEC_TERMINE_VERSION', '1.0.0' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-cec-termine-activator.php
- */
 function activate_cec_termine() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-cec-termine-activator.php';
-	Cec_Termine_Activator::activate();
-}
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-cec-termine-deactivator.php
- */
+}
 function deactivate_cec_termine() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-cec-termine-deactivator.php';
-	Cec_Termine_Deactivator::deactivate();
-}
 
+}
 register_activation_hook( __FILE__, 'activate_cec_termine' );
 register_deactivation_hook( __FILE__, 'deactivate_cec_termine' );
 
 /**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
+ * Enqueue scripts and styles.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-cec-termine.php';
+function termine_scripts() {
+	/* CSS */
+  wp_enqueue_style( 'cec-style', plugin_dir_url( __FILE__ ) . 'public/css/cec-termine-public.css' );
+	/* JS Scripte */
+  wp_enqueue_script( 'cec-js', plugin_dir_url( __FILE__ ) . 'public/js/cec-termine-public.js', array(), '100', true );
+}
+add_action( 'wp_enqueue_scripts', 'termine_scripts' );
 
 /**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
+ * CPT
  */
-function run_cec_termine() {
-
-	$plugin = new Cec_Termine();
-	$plugin->run();
+function create_posttype() {
+  // custom post type Termine
+	register_post_type( 'termine',
+	// CPT Options
+		array(
+			'labels' => array(
+				'name' => __( 'Termine' ),
+				'singular_name' => __( 'Termin' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'termine'),
+      'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields')
+		)
+	);
 
 }
-run_cec_termine();
+add_action( 'init', 'create_posttype' );
+
+/**
+ * -----------------------------------------------------------------------------
+ * Add Shortcodes
+ * -----------------------------------------------------------------------------
+ */
+// [termine_zeigen]
+function termine_zeigen_shortcode($attr, $content) {
+
+    $atts = shortcode_atts( array(
+			'cat' => '',
+      'num' => ''
+		), $atts );
+    ob_start();
+
+		//custom loop options vars
+		$limit = -1;
+		$today = strtotime("now");
+		$datumHeute = date('Ymd', $today);
+		$theorder = "ASC";
+		$feld_abgleich = "termin_datum";
+
+		// define query parameters based on attributes
+		$options = array(
+			 'post_type' => array ( 'termine'),
+		    'posts_per_page' => $limit,
+		    'meta_key' => $feld_abgleich,
+		    'meta_compare' => '>',
+		    'meta_value' => $datumHeute,
+		    'orderby' => $feld_abgleich,
+		    'order' => $theorder
+		);
+
+		$monate = array(
+		            "leer",
+		            "Jan",
+		            "Feb",
+		            "März",
+		            "Apr",
+		            "Mai",
+		            "Juni",
+		            "Juli",
+		            "Aug",
+		            "Sep",
+		            "Okt",
+		            "Nov",
+		            "Dez",
+		        );
+
+		$query = new WP_Query( $options );
+		?>
+    <section class="termine-wrapper">
+      <div class="container">
+
+          <?php
+					// run the loop based on the query
+          if ( $query->have_posts() ) :
+              while ( $query->have_posts() ) : $query->the_post();
+
+								if ( !empty( get_field('termin_datum', $id) ) ) {
+									$termindatum = get_field('termin_datum', $id);
+									$datum = strtotime($termindatum);
+									$monatsindex = date('n', $datum);
+									$tag = date('d.', $datum);
+									$jahr = date('Y', $datum);
+								}
+
+                //$year = get_the_date( 'Y' );
+						    if( ! isset( $years[ $jahr ] ) ) {
+									$years[ $jahr ] = array();
+								}
+						    $years[ $jahr ][] = array(
+										'title' => get_the_title(),
+										'permalink' => get_the_permalink(),
+										'excerpt' => get_the_excerpt(),
+										'monat' => $monate[$monatsindex],
+										'tag' => $tag,
+										'banner' => get_the_post_thumbnail_url(get_the_ID(),'full')
+								);
+
+          		endwhile;
+
+          else :
+
+            get_template_part( 'template-parts/content', 'none' );
+
+          endif;
+
+					foreach ($years as $the_year => $posts) {
+						echo "<h2>$the_year</h2>";
+						foreach ($posts as $index => $post) {
+								echo '<div class="col-4 termine">';
+								if (!empty($post['tag'])) {
+									echo "<div class='datewrapper'>";
+									echo "<span class='bigdate'>".$post['tag']."<br><i>".$post['monat']."</i></span>";
+									echo "</div>";
+								}
+								printf("<a href='%s' class='allover'></a>", $post['permalink'] );
+								printf("<div class='thumbnail' style='background-image:url(%s)'></div>",$post['banner']);
+								echo '<div class="termin-content">';
+								printf("<h4>%s</h4>", $post['title'] );
+								printf("<p>%s</p>", substr($post['excerpt'],0,120) );
+								printf("<span class='cec-more'>%s</span>", "Mehr Infos" );
+								printf("<a href='%s' class='cec-pfeil'></a>", $post['permalink'] );
+								echo '</div>';
+								echo '</div>';
+						}
+					}
+					//echo "<code><pre>";
+					//var_dump($years);
+					//echo "</pre></code>";
+					?>
+			</div>
+		</section>
+
+		<?php
+    $code = ob_get_clean();
+
+    return $code;
+}
+add_shortcode('termine_zeigen', 'termine_zeigen_shortcode');
+
+
+function load_termin_template( $template ) {
+    global $post;
+
+    if ($post->post_type == 'termine') {
+        /*
+         * This is a 'termin' post
+         * AND a 'single termin template' is not found on
+         * theme or child theme directories, so load it
+         * from our plugin directory.
+         */
+        return WP_PLUGIN_DIR  . '/cec-termine/public/single-termin.php';
+    }
+		/*
+		if ( is_singular( 'my_custom_post_type' ) ) {
+			$template = plugins_url( 'templates/my_custom_post_type.php', __FILE__ );
+		}
+		*/
+
+    return $template;
+}
+
+add_filter( 'single_template', 'load_termin_template', 50,1 );
---
+++ public/single-termin.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package cec-termine
+ */
+
+
+ 		$monate = array(
+ 		            "leer",
+ 		            "Jan",
+ 		            "Feb",
+ 		            "März",
+ 		            "Apr",
+ 		            "Mai",
+ 		            "Juni",
+ 		            "Juli",
+ 		            "Aug",
+ 		            "Sep",
+ 		            "Okt",
+ 		            "Nov",
+ 		            "Dez",
+ 		        );
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main singleTermine">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php
+			echo '<header class="entry-header">';
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			echo '</header>';
+		?>
+	<section class="postwrapper">
+		<div class='col-t t-left'>
+			<?php
+        if ( !empty( get_field('termin_datum') ) ) {
+          $termindatum = get_field('termin_datum');
+          $datum = strtotime($termindatum);
+          $datum_format = date('d.m.Y', $datum);
+          $monatsindex = date('n', $datum);
+          $tag = date('d.', $datum);
+          printf("<h5>Datum: %s</h5>", $datum_format );
+        }
+
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'engel' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
+        ?>
+		</div><!-- .t-right -->
+		<div class='col-t t-right'>
+			<?php
+				echo "<div class='datewrapper'>";
+				echo "<span class='bigdate'>".$tag."<br><i>".$monate[$monatsindex]."</i></span>";
+				echo "</div>";
+				the_post_thumbnail("full");
+        ?>
+		</div><!-- .t-left -->
+		<div class="clear"></div>
+	</section>
+</article><!-- #post-<?php the_ID(); ?> -->
+<?php
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
